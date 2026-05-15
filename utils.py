def061f1c8f8d6b09fbd563ba4318a85d6b2b2ca
Le Tan Vi <papylovevip1@gmail.com>
2026-05-15 10:33:42 +0700

    Add main and utils files

---
+++ utils.py
@@ -0,0 +1,6 @@
+def print_menu():
+    print("\n===== STUDENT MANAGEMENT =====")
+    print("1. Them sinh vien")
+    print("2. Hien thi danh sach")
+    print("3. Tim kiem sinh vien")
+    print("0. Thoat")
